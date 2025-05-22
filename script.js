60d603308167d804c6e95d460ada497ac6d60941
amela <huseincehajicamela@gmail.com>
2025-05-22 19:44:30 +0200

    forma za rezervaciju

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JavaScript logika
 
 function potvrdiRezervaciju(){
-    let x = documen.forms("potvrdiRezervaciju")("Rezervacija").value;
+    let x = document.forms("potvrdiRezervaciju")("Rezervacija").value;
     if (x=="") {
         alert ("Name must be filled out");
         return false;
